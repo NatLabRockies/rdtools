74357346359d98e3dfaa46015ac41660a56cce99
cdeline <chris.deline@nrel.gov>
2025-09-18 10:55:26 -0600

    Try again to solve pandas3.0 futurewarning

--- rdtools/degradation.py
+++ rdtools/degradation.py
@@ -330,11 +330,12 @@ def degradation_year_on_year(energy_normalized, recenter=True,
     yoy_result.index = YoY_times[f'dt_{label}']
     yoy_result.index.name = None
 
-    with pd.option_context('future.no_silent_downcasting', True):
-        # the following is throwing a warning without the above context manager.
-        # see https://github.com/pandas-dev/pandas/issues/57734
-        energy_normalized = energy_normalized.merge(usage_of_points, how='left', left_on='dt',
-                                                    right_index=True, left_index=False).fillna(0.0)
+    # with pd.option_context('future.no_silent_downcasting', True):
+    # the following is throwing a warning without the above context manager.
+    # see https://github.com/pandas-dev/pandas/issues/57734
+    energy_normalized = energy_normalized.merge(usage_of_points, how='left', left_on='dt',
+                                                right_index=True, left_index=False
+                                                ).fillna(0.0).infer_objects(copy=False)
 
     if uncertainty_method == 'simple':  # If we need the full results
         calc_info = {
